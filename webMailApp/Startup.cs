5e7ad1a39f098b01555ede0f1b31f7bf2489c586
Frank Cumaio <Frank Cumaio@DESKTOP-CKV44ER>
2016-11-15 09:11:33 +0200

    Add project files.

---
+++ webMailApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webMailApp.Startup))]
+namespace webMailApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
